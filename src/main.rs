50d02349b9ffcb3d05e265fa1a592301a5014613
Shane Lillie <ignignokterr@gmail.com>
2023-10-25 17:03:15 -0700

    Working on task types and real dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,50 @@
+use std::sync::atomic::{AtomicUsize, Ordering::SeqCst};
+
 use futures::FutureExt;
 use rand::prelude::*;
 use tokio::sync::watch;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+static RUNNER_IDS: AtomicUsize = AtomicUsize::new(0);
+
+#[derive(Debug, Default, Copy, Clone, PartialEq, Eq)]
 enum Status {
+    #[default]
     NotStarted,
+    Waiting,
     Running,
     Complete,
 }
 
 #[derive(Debug)]
-struct Runner {
-    tx: watch::Sender<Status>,
-}
-
-impl Default for Runner {
-    fn default() -> Self {
-        let (tx, _) = watch::channel(Status::NotStarted);
-        Self { tx }
-    }
-}
-
-impl Runner {
-    fn subscribe(&self) -> watch::Receiver<Status> {
-        self.tx.subscribe()
-    }
-
-    async fn run(&self) {
-        let mut rng = rand::thread_rng();
-        let wait = rng.gen_range(5..10);
-
-        self.tx.send_replace(Status::Running);
-        println!("running ({}) ...", wait);
-
-        tokio::time::sleep(tokio::time::Duration::from_secs(wait)).await;
-
-        println!("complete!");
-        self.tx.send_replace(Status::Complete);
-    }
+enum TaskType {
+    Create,
+    Destroy,
+    Update,
 }
 
 #[derive(Debug)]
-struct Waiter {
+struct Runner {
+    id: usize,
+
+    task_type: TaskType,
+
+    tx: watch::Sender<Status>,
     dependencies: Vec<watch::Receiver<Status>>,
 }
 
-impl Waiter {
-    fn new(dependencies: Vec<watch::Receiver<Status>>) -> Self {
-        Self { dependencies }
+impl Runner {
+    fn new(task_type: TaskType, dependencies: Vec<watch::Receiver<Status>>) -> Self {
+        let (tx, _) = watch::channel(Status::default());
+        Self {
+            id: RUNNER_IDS.fetch_add(1, SeqCst),
+            task_type,
+            tx,
+            dependencies,
+        }
+    }
+
+    fn subscribe(&self) -> watch::Receiver<Status> {
+        self.tx.subscribe()
     }
 
     async fn wait_for_dependencies(&mut self) {
@@ -56,43 +53,57 @@ impl Waiter {
             tasks.push(rx.wait_for(|status| *status == Status::Complete));
         }
 
-        println!("waiting for dependencies ...");
+        println!(
+            "[{} {:?}]: waiting for dependencies ...",
+            self.id, self.task_type
+        );
         futures::future::join_all(tasks).await;
-        println!("dependencies finished!");
+        println!("[{} {:?}]: dependencies finished!", self.id, self.task_type);
+    }
+
+    async fn run(&mut self) {
+        self.tx.send_replace(Status::Waiting);
+        self.wait_for_dependencies().await;
+
+        let mut rng = rand::thread_rng();
+        let wait = rng.gen_range(5..10);
+
+        self.tx.send_replace(Status::Running);
+        println!("[{} {:?}]: running ({}) ...", self.id, self.task_type, wait);
+
+        tokio::time::sleep(tokio::time::Duration::from_secs(wait)).await;
+
+        println!("[{} {:?}]: complete!", self.id, self.task_type);
+        self.tx.send_replace(Status::Complete);
     }
 }
 
 #[derive(Debug)]
 struct App {
     runners: Vec<Runner>,
-    waiter: Waiter,
 }
 
 impl Default for App {
     fn default() -> Self {
         let mut runners = vec![];
-        let mut dependencies = vec![];
+        //let mut dependencies = vec![];
 
         for _ in 0..10 {
-            let runner = Runner::default();
-            dependencies.push(runner.subscribe());
+            let runner = Runner::new(TaskType::Create, vec![]);
+            //dependencies.push(runner.subscribe());
             runners.push(runner);
         }
 
-        Self {
-            runners,
-            waiter: Waiter::new(dependencies),
-        }
+        Self { runners }
     }
 }
 
 impl App {
     async fn run(&mut self) {
         let mut tasks = vec![];
-        for runner in &self.runners {
+        for runner in &mut self.runners {
             tasks.push(runner.run().boxed_local());
         }
-        tasks.push(self.waiter.wait_for_dependencies().boxed_local());
 
         futures::future::join_all(tasks).await;
     }
