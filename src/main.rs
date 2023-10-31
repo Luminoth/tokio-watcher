18399506fc6d2e90a55c5dac9925c4dd08d486d7
Shane Lillie <ignignokterr@gmail.com>
2023-10-31 15:22:33 -0700

    Working through dependency garbage

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use futures::FutureExt;
 use rand::prelude::*;
 use tokio::sync::watch;
 
-static RUNNER_IDS: AtomicUsize = AtomicUsize::new(0);
+static THING_IDS: AtomicUsize = AtomicUsize::new(0);
 
 #[derive(Debug, Default, Copy, Clone, PartialEq, Eq)]
-enum Status {
+enum TaskStatus {
     #[default]
     NotStarted,
     Waiting,
@@ -16,93 +16,238 @@ enum Status {
 }
 
 #[derive(Debug)]
+struct TaskState {
+    tx: watch::Sender<TaskStatus>,
+    dependencies: Vec<watch::Receiver<TaskStatus>>,
+}
+
+impl TaskState {
+    fn new(dependencies: Vec<watch::Receiver<TaskStatus>>) -> Self {
+        let (tx, _) = watch::channel(TaskStatus::default());
+        Self { tx, dependencies }
+    }
+
+    fn has_dependencies(&self) -> bool {
+        !self.dependencies.is_empty()
+    }
+
+    fn update_status(&self, status: TaskStatus) -> TaskStatus {
+        self.tx.send_replace(status)
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
 enum TaskType {
-    Create,
     Destroy,
+    Create,
     Update,
 }
 
 #[derive(Debug)]
-struct Runner {
-    id: usize,
-
-    task_type: TaskType,
-
-    tx: watch::Sender<Status>,
-    dependencies: Vec<watch::Receiver<Status>>,
+enum Task<T>
+where
+    T: Thing,
+{
+    Destroy(T, TaskState),
+    Create(T, TaskState),
+    Update(T, T, TaskState),
 }
 
-impl Runner {
-    fn new(task_type: TaskType, dependencies: Vec<watch::Receiver<Status>>) -> Self {
-        let (tx, _) = watch::channel(Status::default());
-        Self {
-            id: RUNNER_IDS.fetch_add(1, SeqCst),
-            task_type,
-            tx,
-            dependencies,
+impl<T> Task<T>
+where
+    T: Thing,
+{
+    fn get_id(&self) -> usize {
+        match self {
+            Self::Destroy(thing, _) => thing.get_id(),
+            Self::Create(thing, _) => thing.get_id(),
+            Self::Update(thing, _, _) => thing.get_id(),
         }
     }
 
-    fn subscribe(&self) -> watch::Receiver<Status> {
-        self.tx.subscribe()
+    fn get_task_type(&self) -> TaskType {
+        match self {
+            Self::Destroy(_, _) => TaskType::Destroy,
+            Self::Create(_, _) => TaskType::Create,
+            Self::Update(_, _, _) => TaskType::Update,
+        }
+    }
+
+    fn get_thing_type(&self) -> ThingType {
+        match self {
+            Self::Destroy(thing, _) => thing.get_type(),
+            Self::Create(thing, _) => thing.get_type(),
+            Self::Update(thing, _, _) => thing.get_type(),
+        }
+    }
+
+    fn get_state(&self) -> &TaskState {
+        match self {
+            Self::Destroy(_, state) => state,
+            Self::Create(_, state) => state,
+            Self::Update(_, _, state) => state,
+        }
+    }
+
+    fn get_state_mut(&mut self) -> &mut TaskState {
+        match self {
+            Self::Destroy(_, state) => state,
+            Self::Create(_, state) => state,
+            Self::Update(_, _, state) => state,
+        }
+    }
+
+    fn subscribe(&self) -> watch::Receiver<TaskStatus> {
+        self.get_state().tx.subscribe()
     }
 
     async fn wait_for_dependencies(&mut self) {
+        let id = self.get_id();
+        let task_type = self.get_task_type();
+        let thing_type = self.get_thing_type();
+
         let mut tasks = vec![];
-        for rx in self.dependencies.iter_mut() {
-            tasks.push(rx.wait_for(|status| *status == Status::Complete));
+        for rx in self.get_state_mut().dependencies.iter_mut() {
+            tasks.push(rx.wait_for(|status| *status == TaskStatus::Complete));
         }
 
         println!(
-            "[{} {:?}]: waiting for dependencies ...",
-            self.id, self.task_type
+            "[{} {:?} {:?}]: waiting for dependencies ...",
+            id, task_type, thing_type
         );
         futures::future::join_all(tasks).await;
-        println!("[{} {:?}]: dependencies finished!", self.id, self.task_type);
+        println!(
+            "[{} {:?} {:?}]: dependencies finished!",
+            id, task_type, thing_type
+        );
     }
 
     async fn run(&mut self) {
-        self.tx.send_replace(Status::Waiting);
-        self.wait_for_dependencies().await;
+        let id = self.get_id();
+        let task_type = self.get_task_type();
+        let thing_type = self.get_thing_type();
+
+        if self.get_state().has_dependencies() {
+            self.get_state().update_status(TaskStatus::Waiting);
+            self.wait_for_dependencies().await;
+        }
+
+        self.get_state().update_status(TaskStatus::Running);
 
         let mut rng = rand::thread_rng();
         let wait = rng.gen_range(5..10);
 
-        self.tx.send_replace(Status::Running);
-        println!("[{} {:?}]: running ({}) ...", self.id, self.task_type, wait);
+        println!(
+            "[{} {:?} {:?}]: running ({}) ...",
+            id, task_type, thing_type, wait
+        );
 
         tokio::time::sleep(tokio::time::Duration::from_secs(wait)).await;
 
-        println!("[{} {:?}]: complete!", self.id, self.task_type);
-        self.tx.send_replace(Status::Complete);
+        println!("[{} {:?} {:?}]: complete!", id, task_type, thing_type);
+
+        self.get_state().update_status(TaskStatus::Complete);
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum ThingType {
+    ThingOne,
+    ThingTwo,
+}
+
+trait Thing {
+    fn get_id(&self) -> usize;
+
+    fn get_type(&self) -> ThingType;
+}
+
+#[derive(Debug)]
+struct ThingOne {
+    id: usize,
+}
+
+impl Thing for ThingOne {
+    fn get_id(&self) -> usize {
+        self.id
+    }
+
+    fn get_type(&self) -> ThingType {
+        ThingType::ThingOne
+    }
+}
+
+impl ThingOne {
+    fn new() -> Self {
+        Self {
+            id: THING_IDS.fetch_add(1, SeqCst),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct ThingTwo {
+    id: usize,
+}
+
+impl Thing for ThingTwo {
+    fn get_id(&self) -> usize {
+        self.id
+    }
+
+    fn get_type(&self) -> ThingType {
+        ThingType::ThingTwo
+    }
+}
+
+impl ThingTwo {
+    fn new() -> Self {
+        Self {
+            id: THING_IDS.fetch_add(1, SeqCst),
+        }
     }
 }
 
 #[derive(Debug)]
 struct App {
-    runners: Vec<Runner>,
+    thing_one_tasks: Vec<Task<ThingOne>>,
+    thing_two_tasks: Vec<Task<ThingTwo>>,
 }
 
 impl Default for App {
     fn default() -> Self {
-        let mut runners = vec![];
-        //let mut dependencies = vec![];
-
+        let mut thing_one_tasks = vec![];
         for _ in 0..10 {
-            let runner = Runner::new(TaskType::Create, vec![]);
-            //dependencies.push(runner.subscribe());
-            runners.push(runner);
+            let task = Task::Create(ThingOne::new(), TaskState::new(vec![]));
+            thing_one_tasks.push(task);
         }
 
-        Self { runners }
+        let mut thing_two_tasks = vec![];
+        for i in 0..10 {
+            let task = Task::Create(
+                ThingTwo::new(),
+                TaskState::new(vec![thing_one_tasks[i].subscribe()]),
+            );
+            thing_two_tasks.push(task);
+        }
+
+        Self {
+            thing_one_tasks,
+            thing_two_tasks,
+        }
     }
 }
 
 impl App {
     async fn run(&mut self) {
         let mut tasks = vec![];
-        for runner in &mut self.runners {
-            tasks.push(runner.run().boxed_local());
+
+        for task in &mut self.thing_one_tasks {
+            tasks.push(task.run().boxed_local());
+        }
+
+        for task in &mut self.thing_two_tasks {
+            tasks.push(task.run().boxed_local());
         }
 
         futures::future::join_all(tasks).await;
